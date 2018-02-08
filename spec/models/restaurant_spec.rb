f511afbb9559e45f3f73e84bc8cf16f93dc81902
Houston Charles <houstoncharles@Houstons-MacBook-Air.local>
2018-02-08 16:11:29 -0500

    Updated spec test.

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -4,18 +4,15 @@ RSpec.describe Restaurant, type: :model do
   it { should have_valid(:name).when("good burger") }
   it { should_not have_valid(:name).when("") }
 
-  it { should have_valid(:address).when("500 summer street") }
-  it { should_not have_valid(:address).when("")}
+  it { should have_valid(:price).when("$$") }
+  it { should_not have_valid(:price).when("") }
 
-  it { should have_valid(:city).when("Boston") }
-  it { should_not have_valid(:city).when("") }
+  it { should have_valid(:rating).when("2") }
+  it { should_not have_valid(:rating).when("") }
 
-  it { should have_valid(:state).when("MA")}
-  it { should_not have_valid(:state).when("") }
+  it { should have_valid(:display_phone).when("123456789") }
+  it { should_not have_valid(:display_phone).when("")}
 
-  it { should have_valid(:zip).when("00000") }
-  it { should_not have_valid(:zip).when("0000") }
-
-  it { should have_valid(:picture).when("https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Hamburger_%28black_bg%29.jpg/1200px-Hamburger_%28black_bg%29.jpg") }
-  it { should_not have_valid(:picture).when("") }
+  it { should have_valid(:image_url).when("https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Hamburger_%28black_bg%29.jpg/1200px-Hamburger_%28black_bg%29.jpg") }
+  it { should_not have_valid(:image_url).when("") }
 end
