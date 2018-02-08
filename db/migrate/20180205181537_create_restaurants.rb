176a45b7752ac11f589371a20ce7449fb05090e2
Houston Charles <houstoncharles@Houstons-MacBook-Air.local>
2018-02-08 16:05:20 -0500

    Added functionality for like and dislike buttons and saving data.

--- db/migrate/20180205181537_create_restaurants.rb
+++ db/migrate/20180205181537_create_restaurants.rb
@@ -2,12 +2,12 @@ class CreateRestaurants < ActiveRecord::Migration[5.1]
   def change
     create_table :restaurants do |t|
       t.string :name, null: false
-      t.string :picture, null: false
-      t.string :address, null: false
-      t.string :city, null: false
-      t.string :state, null: false
-      t.string :zip, null: false
-      t.text :description
+      t.string :image_url, null: false
+      t.string :price, null: false
+      t.string :rating, null: false
+      t.string :display_phone, null: false
+      t.string :categories, null: false, array: true
+      t.string :location, null: false
       t.timestamps
     end
   end
