37cbf8352b731852e44dd9a398f6218ad1b4725e
Houston Charles <houstoncharles@Houstons-MacBook-Air.local>
2018-02-12 11:21:38 -0500

    Refactored so that only the home page logic was contained here.

--- app/javascript/packs/components/HomePageComponent.js
+++ app/javascript/packs/components/HomePageComponent.js
@@ -1,63 +1,27 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router';
 
 class HomePageComponent extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      user: true,
-      searchText: '',
+      user: false
     }
-    this.searchSubmit = this.searchSubmit.bind(this)
-    this.searchTextChange = this.searchTextChange.bind(this)
-  }
-
-  searchTextChange(event) {
-    this.setState({searchText: event.target.value});
-  }
-
-  searchSubmit(event) {
-    event.preventDefault()
-    let payload = this.state.searchText
-    // fetch('/api/restaurants?loc=' + payload, {
-    //   method: 'GET',
-    //   credentials: 'same-origin',
-    //   headers: {
-    //    'Content-Type': 'application/json',
-    //    'X-Requested-With': 'XMLHttpRequest'
-    //   },
-    // })
-    debugger;
-    window.location = '/restaurants/' + payload;
-    // this.props.history.push(`/restaurants/${payload}`)
   }
 
   componentDidMount() {
+    debugger;
     fetch('/api/homes', {credentials: 'same-origin'})
       .then(response => response.json())
       .then(body => {
         let logInStatus = body
         this.setState({ user: logInStatus })
-        debugger;
       })
+      if (this.state.user == true){
+        window.location = "/restaurants";
+      }
   }
 
   render() {
-    let conditionalDisplay;
-    let searchBar = (
-      <form onSubmit={this.searchSubmit} className='search'>
-        <label> Let's get started! Enter your location.
-          <input
-            name="searchText"
-            type="text"
-            value={this.state.searchText}
-            onChange={this.searchTextChange}
-            placeholder="Location"
-          />
-        </label>
-        <input className="button" type="submit" value="Submit"/>
-      </form>
-    )
     let LoginPage = (
         <div>
           <a href='users/sign_in'><button type='button'> Log In </button></a>
@@ -65,14 +29,9 @@ class HomePageComponent extends Component {
           <a href='users/sign_up'><button type='button'> Sign Up </button></a>
         </div>
     )
-    if (this.state.user == true){
-      conditionalDisplay = searchBar;
-    } else {
-      conditionalDisplay = LoginPage;
-    }
     return(
       <div>
-        {conditionalDisplay}
+        {LoginPage}
       </div>
     )
   }
