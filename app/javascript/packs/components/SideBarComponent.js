805ce1cfcf9f4a3a78f3961ef607e880444dba0b
Houston Charles <houstoncharles@Houstons-MacBook-Air.local>
2018-02-16 10:40:02 -0500

    Refactored to make SideBarComponent contains most state and functions.

--- app/javascript/packs/components/SideBarComponent.js
+++ app/javascript/packs/components/SideBarComponent.js
@@ -5,41 +5,17 @@ class SideBarComponent extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      matches: [],
-      location: ''
+      matches: []
     }
-    this.deleteMatch = this.deleteMatch.bind(this);
-  }
-
-  componentDidMount() {
-    fetch('/api/matches', {credentials: 'same-origin'})
-      .then(response => response.json())
-      .then(body => {
-        let returned_matches = body
-        this.setState({ matches: returned_matches })
-      })
-  }
-
-  deleteMatch(current_match){
-    fetch('/api/matches/' + current_match, {
-      method: 'DELETE',
-      credentials: 'same-origin',
-      headers: {
-       'Content-Type': 'application/json',
-       'X-Requested-With': 'XMLHttpRequest'
-      }
-    }).then((response) => {
-      const newMatches = this.state.matches.filter(match => match.id !== current_match)
-      this.setState({matches: newMatches})
-    })
   }
 
   render(){
-    let matches = this.state.matches.map(Match => {
+    let matches = this.props.matches.map(Match => {
       return(
-        <div id="match"> {Match.name} <span onClick={() => this.deleteMatch(Match.id)} className='ecks'><i className="fas fa-times"></i></span></div>
+        <div id="match"> {Match.name} <span onClick={() => this.props.handleDelete(Match.id)} className='ecks'><i className="fas fa-times"></i></span></div>
       )
     })
+
     return(
       <div>
         <Menu id='sideBar'>
--- app/javascript/packs/containers/SearchBarContainer.js
+++ app/javascript/packs/containers/SearchBarContainer.js
@@ -9,38 +9,118 @@ class SearchBarContainer extends Component {
     super(props);
     this.state = {
       searchText: '',
-      location: ''
+      location: '',
+      matches: [],
+      repeats: [],
+      restaurant: {}
     }
     this.searchSubmit = this.searchSubmit.bind(this)
     this.searchTextChange = this.searchTextChange.bind(this)
+    this.deleteMatch = this.deleteMatch.bind(this)
+    this.initialRender = this.initialRender.bind(this)
+    this.onLike = this.onLike.bind(this)
+    this.onDisLike = this.onDisLike.bind(this)
+    this.getMatches = this.getMatches.bind(this)
 
-    // this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
   }
-
-  showSettings (event) {
-    event.preventDefault();
-  }
-
-  // onSetSidebarOpen(open){
-  //   this.setState({sidebarOpen: open});
-  // }
 
   searchTextChange(event) {
     this.setState({searchText: event.target.value});
   }
 
   searchSubmit(event) {
-    event.preventDefault()
-    this.setState({location: this.state.searchText})
+    event.preventDefault();
+    this.setState({location: this.state.searchText});
+  }
+
+  getMatches(){
+    fetch('/api/matches', {credentials: 'same-origin'})
+    .then(response => response.json())
+    .then(body => {
+      let returned_matches = body
+      this.setState({ matches: returned_matches })
+      debugger;
+    })
   }
 
   componentDidMount() {
-    fetch('/api/homes', {credentials: 'same-origin'})
+    fetch('/api/matches', {credentials: 'same-origin'})
+    .then(response => response.json())
+    .then(body => {
+      let returned_matches = body
+      this.setState({ matches: returned_matches })
+    })
+  }
+
+  initialRender(){
+    let payload = this.state.location
+    fetch('/api/restaurants.json?loc=' + payload, {
+      method: 'GET',
+      credentials: 'same-origin',
+      headers: {
+       'Content-Type': 'application/json',
+       'X-Requested-With': 'XMLHttpRequest'
+      }
+    })
+    .then(response => response.json())
+    .then(body => {
+      let jsonRestaurant = body
+      this.setState({ restaurant: jsonRestaurant })
+    })
+  }
+
+  onLike(event){
+    let payload= {restaurant: this.state.restaurant}
+    fetch('/api/restaurants.json', {
+      method: 'POST',
+      body: JSON.stringify(payload),
+      credentials: 'same-origin',
+      headers: {
+       'content-type': 'application/json',
+       'X-Requested-With': 'XMLHttpRequest'
+      },
+    })
+    this.onDisLike(event);
+  }
+
+  onDisLike(event){
+    event.preventDefault();
+    let jsonRestaurant;
+    let payload = this.state.location
+    fetch('/api/restaurants.json?loc=' + payload, {
+      method: 'GET',
+      credentials: 'same-origin',
+      headers: {
+       'Content-Type': 'application/json',
+       'X-Requested-With': 'XMLHttpRequest'
+      }
+    })
       .then(response => response.json())
       .then(body => {
-        let logInStatus = body
-        this.setState({ user: logInStatus })
+        if (this.state.repeats.includes(body.id)){
+          this.onDisLike(event)
+        } else {
+          jsonRestaurant = body
+          let previousState = this.state.repeats
+          this.setState({repeats: previousState.concat(body.id)})
+          this.setState({ restaurant: jsonRestaurant })
+          this.getMatches();
+        }
       })
+  }
+
+  deleteMatch(current_match){
+    fetch('/api/matches/' + current_match, {
+      method: 'DELETE',
+      credentials: 'same-origin',
+      headers: {
+       'Content-Type': 'application/json',
+       'X-Requested-With': 'XMLHttpRequest'
+      }
+    }).then((response) => {
+      const newMatches = this.state.matches.filter(match => match.id !== current_match)
+      this.setState({matches: newMatches})
+    })
   }
 
   render() {
@@ -64,11 +144,17 @@ class SearchBarContainer extends Component {
       <div>
         <RestaurantShowContainer
           location={this.state.location}
+          initialRender={this.initialRender}
+          handleLike={this.onLike}
+          handleDisLike={this.onDisLike}
+          restaurant={this.state.restaurant}
         />
       </div>
     )
     let sideBar = (
       <SideBarComponent
+        matches={this.state.matches}
+        handleDelete={this.deleteMatch}
       />
     )
     if (this.state.location != '')
