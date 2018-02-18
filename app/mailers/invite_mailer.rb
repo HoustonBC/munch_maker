93f6f14aaef2d006bdffb290bdbdef834f4d8864
Houston Charles <houstoncharles@Houstons-MacBook-Air.local>
2018-02-18 17:38:47 -0500

    added email functionality.

--- app/mailers/invite_mailer.rb
+++ app/mailers/invite_mailer.rb
@@ -1,5 +1,12 @@
 class InviteMailer < ApplicationMailer
-  def new_invite(invite)
-    # Build the email in here...
+  def new_invite(event, recipient, sender)
+    @recipient = recipient
+    @event = event
+    @sender = sender
+
+    mail(
+      to: recipient,
+      subject: "new invite from #{sender}"
+    )
   end
 end
