91b73c188535a5243505cff7f73147553e833252
Houston Charles <houstoncharles@Houstons-MacBook-Air.local>
2018-04-01 19:10:05 -0400

    Added spec test for User API

---
+++ spec/controllers/api/users_controller_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.describe Api::UsersController, type: :controller do
+  let!(:user) {User.create!(email: "b@b.com", password: "password")}
+  describe "GET#index" do
+    it "should return a list of all the users" do
+
+      get :index
+      returned_json = JSON.parse(response.body)
+
+      expect(response.status).to eq 200
+      expect(response.content_type).to eq("application/json")
+      expect(returned_json.length).to eq 1
+      expect(returned_json[0]["email"]).to eq "b@b.com"
+    end
+  end
+end
